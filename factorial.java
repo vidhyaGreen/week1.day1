5c2b2a3e9eea364c102b44e7b9d2400a0770fcec
vidhyaGreen <92054443+vidhyaGreen@users.noreply.github.com>
2021-10-07 08:47:31 -0700

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,15 @@
+package week1assignment;
+
+public class factorial {
+
+public static void main(String[] args) {
+	int a, fact=1;
+	int number=5;
+	for(a=1;a<=number;a++)
+	{
+	fact=fact*a;
+	}
+System.out.println("the factiral of 5 is" + fact);
+	}
+
+}
