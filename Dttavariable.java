cb3f0491cab59bfdd14ca34ed1665752f60d97f1
vidhyaGreen <92054443+vidhyaGreen@users.noreply.github.com>
2021-10-07 09:42:47 -0700

    Add files via upload

---
+++ Dttavariable.java
@@ -0,0 +1,14 @@
+package week1assignment;
+
+public class Dttavariable {
+
+	public static void main(String[] args) {
+Float size=14.5f;
+boolean isTouchScreen=true;
+char brandname='s';
+int resolution=455;
+System.out.println("The Size Of the Mobile" +   size);
+System.out.println(" the brandname is" +   brandname);
+System.out.println("the resolution is " +resolution);
+}
+}
